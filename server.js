b491c2684f7d5046103aedd6ee8edf7381be7658
Mauricio Fow <mauriciofow@gmail.com>
2019-10-02 15:24:23 -0500

    testing headers

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const { readFileSync } = require('fs');
 const { join } = require('path');
 const cors = require('cors');
 const { ApolloServer, gql } = require('apollo-server');
-
+const jwt = require('jsonwebtoken');
 const resolvers = require('./lib/resolvers');
-const isDev = process.env.NODE_ENV !== 'production';
-const path = require('path');
 
 // definiendo le esquema
 const typeDefs = readFileSync(
@@ -20,7 +18,31 @@ const typeDefs = readFileSync(
 	'utf-8'
 );
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context   : ({ req }) => {
+		// get the user token from the headers
+		const data = { usuario: req.headers.usuario, password: req.headers.password };
+		const token = jwt.sign({ sub: data.usuario, password: data.password }, process.env.JWT_SECRET_TOKEN);
+		console.log(token);
+
+		let loggedIn = true;
+		try {
+			const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
+			const user = decoded.sub;
+			// optionally block the user
+			// we could also check user roles/permissions here
+			if (!user) loggedIn = false;
+
+			// add the user to the context
+			return { loggedIn };
+		} catch (err) {
+			loggedIn = false;
+			return { loggedIn };
+		}
+	}
+});
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
 	console.log(`🚀 Server ready at ${url}`);
 });
