577c85d4b36e6773a168530b0e552af3a845023e
dekker <mauriciofow@gmail.com>
2019-10-15 14:25:02 -0500

    update quantity mutation

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ const server = new ApolloServer({
     }
   }
 });
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+server.listen({ port: process.env.PORT || 5000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
